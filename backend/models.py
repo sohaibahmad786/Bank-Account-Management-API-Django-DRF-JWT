6cf7927eed6ba294936fcefe8917a70d9fafd335
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-02-27 15:48:09 +0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Student(models.Model):
-    name=models.CharField()
-    email=models.EmailField()
-    age=models.IntegerField()
-    course=models.CharField()
-    created_at=models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Account(AbstractUser):
     full_name=models.CharField()
     age=models.IntegerField()
