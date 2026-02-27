4ec35e27ee24f7fd508f98d314ac01e98ee29124
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-02-27 15:53:39 +0500

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -9,29 +9,17 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.authentication import TokenAuthentication,SessionAuthentication
 from rest_framework import permissions,generics
 from rest_framework.permissions import IsAuthenticated,AllowAny
-from .models import Student
-from .serializer import Student_serilizer
 from .models import Account
 from .serializer import Account_serilizer
 from django.conf import settings
 from datetime import datetime, timedelta
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-  
-# ______________________ Student Managment API___________________
-class student_list(generics.ListCreateAPIView):
-    queryset=Student.objects.all()
-    serializer_class=Student_serilizer
-class student_detail(generics.RetrieveUpdateDestroyAPIView):
-    queryset=Student.objects.all()
-    serializer_class=Student_serilizer
-    
 
 # _____________________ Banking Account API __________
 class account_list(generics.ListCreateAPIView):
     queryset=Account.objects.all()
     serializer_class=Account_serilizer
-    authentication_classes=[JWTAuthentication]
     permission_classes=[permissions.AllowAny]
 
 class account_detail(generics.RetrieveUpdateDestroyAPIView):
