14fa9fe880f5f3afd68a0a91ec8ff599294650a3
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-02-27 15:55:29 +0500

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 
-from .models import Student
 from .models import Account
-
 admin.site.register(Account)
-admin.site.register(Student)
 # Register your models here.
